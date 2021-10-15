5a0188e9a152762558d4679bc9b3135eeeed8af0
hassanplz <hassan.albujasim@ucdconnect.ie>
2021-10-15 14:53:14 +0100

    removing file

--- l2-Jax-WS/ignore/quote-ws-pf-cf/client-cf/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import quote.*;
-
-public class Main {
-    public static void main(String[] args) {
-        StockQuoteService service = new StockQuoteService();
-        StockQuote stockQuote = service.getStockQuotePort();
-        System.out.println("IBM: " + stockQuote.getStockPrice("IBM"));
-    }
-}
